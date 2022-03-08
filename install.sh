1d2feb62fa50c3e13684a5608ba28e2c8e80da55
PeakyBlinders <93531354+DonatoReis@users.noreply.github.com>
2022-03-08 19:28:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=1.0.6
+version=1.0.4
 usage() {
   printf 'MSG\n'
 }
@@ -37,7 +37,7 @@ in_array() {
 
 print_message() {
   if [[ $* ]]; then
-    message_fmt="\n\n${CBold}${CFGCyan}[${CFGWhite}+${CFGCyan}] %s${CReset}\n"
+    message_fmt="\n\n${CBold}${CFGYellow}[${CFGRed}+${CFGYellow}] %s${CReset}\n"
     printf "$message_fmt" "$*"
   fi
 }
@@ -59,8 +59,8 @@ banner() {
 ██╔══██║██╔══██╗██║╚██╗██║██║   ██║
 ██║  ██║██║  ██║██║ ╚████║╚██████╔╝
 ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝'
-  social="  A Reconaissance Tool's Collection.
-  
+  social="    A Reconaissance Tool's Collection.
+ 
 ╔════════════════════════════════════╗
 ║                                    ║
 ║ https://t.me/PeakyBlindersW        ║
@@ -86,11 +86,11 @@ system_update() {
 export -f system_update
 
 system_upgrade() {
-  print_message 'Updating system'
+  print_message 'Atualizando sistema'
   apt -y full-upgrade
   sudo $SUDO_OPT pip3 install --upgrade pip
   sudo $SUDO_OPT pip3 install --upgrade osrframework
-  apt -y update && apt -y autoremove && apt -y autoclean
+  apt -y autoremove
 }
 
 check_dependencies() {
@@ -117,20 +117,17 @@ init_install() {
   mkdir -p "$srcdir"
   system_update
   # REQUIREMENTS
+  print_message 'Ferramenta em script Bash Completa para Bug bounty ou Pentest ! Vai poupar seu Tempo na hora de configurar sua máquina para trabalhar.'
+  printf "\n${CBold}${CFGWhite}=====================================================>${CReset}\n\n"
   if [[ ! -f $HOME/.local/.arno_init_install_successful ]]; then
-    print_message 'Complete tool for Bug bounty and Pentest! Less time when setting up your machine to work.'
-    printf "\n${CBold}${CFGWhite}┃════════════════════════════════════════════════════════════════════>>>${CReset}\n\n"
-    apt -y install python3-pip apt-transport-https curl libcurl4-openssl-dev libssl-dev virtualbox-guest-x11 jq ruby-full libcurl4-openssl-dev ruby virtualbox-guest-utils libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev hcxtools hcxdumptool zlib1g-dev perl chromium libio-socket-ssl-perl libdbd-sqlite3-perl libclass-dbi-perl libio-all-lwp-perl libparallel-forkmanager-perl htop libredis-perl libalgorithm-combinatorics-perl gem git cvs subversion git bzr mercurial build-essential libssl-dev libffi-dev python2-dev python2 python-dev-is-python3 ruby-ffi-yajl libldns-dev nmap rename docker.io parsero apache2 amass feroxbuster joomscan uniscan ssh tor privoxy wifite proxychains4 hashcat aptitude synaptic lolcat python3.9-venv dialog golang-go exploitdb exploitdb-papers exploitdb-bin-sploits graphviz virtualenv reaver bats metagoofil openssl
+    apt -y install python3-pip apt-transport-https curl libcurl4-openssl-dev libssl-dev virtualbox-guest-x11 jq ruby-full libcurl4-openssl-dev ruby virtualbox-guest-utils libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev hcxtools  hcxdumptool zlib1g-dev perl chromium libio-socket-ssl-perl libdbd-sqlite3-perl libclass-dbi-perl libio-all-lwp-perl libparallel-forkmanager-perl libredis-perl libalgorithm-combinatorics-perl gem git cvs subversion git bzr mercurial build-essential libssl-dev libffi-dev python2-dev python2 python-dev-is-python3 ruby-ffi-yajl python-setuptools libldns-dev nmap rename docker.io parsero apache2 amass joomscan uniscan ssh tor privoxy wifite proxychains4 hashcat aptitude synaptic lolcat python3.9-venv dialog golang-go exploitdb exploitdb-papers exploitdb-bin-sploits graphviz virtualenv reaver bats metagoofil openssl dirsearch
     sudo $SUDO_OPT pip3 install --upgrade pip
-    sudo $SUDO_OPT pip3 install argparse osrframework py-altdns==1.0.2 requests wfuzz holehe twint bluto droopescan uro
+    sudo $SUDO_OPT pip3 install argparse osrframework py-altdns==1.0.2 requests wfuzz holehe twint bluto droopescan
     sudo $SUDO_OPT pip install one-lin3r bluto dnspython requests win_unicode_console colorama netaddr
     gem install typhoeus opt_parse_validator blunder wpscan
     mkdir -p "$HOME/.local"
     > $HOME/.local/.arno_init_install_successful
   fi
-
-  print_message 'Complete tool for Bug bounty and Pentest! Less time when setting up your machine to work.'
-  printf "\n${CBold}${CFGWhite}┃════════════════════════════════════════════════════════════════════>>>${CReset}\n\n"
 }
 
 progressbar() {
@@ -249,7 +246,7 @@ for tool in ${selection,,}; do
   if in_array "$tool" ${tool_list,,}; then
     export url script
     IFS='|' read url script post_install <<< "${tools[$tool]}"
-    print_message "Installing ${tool^}"
+    print_message "Instalando ${tool^}"
     [[ $url ]] && git_install "$url" "$script"
     [[ $post_install ]] && {
       result=$(bash -c "$post_install" 2>>$logerr >>$logfile) | progressbar -s normal -m "${tool^}: Installation"
