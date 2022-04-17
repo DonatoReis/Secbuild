03c061dfeb15f0c99ac2e292b2507083b304de35
PeakyBlinder <93531354+DonatoReis@users.noreply.github.com>
2022-04-17 16:38:14 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -149,7 +149,8 @@ init_install() {
         ;;
     esac
     apt -y install $packages
-    pip3 install --upgrade pip osrframework py-altdns==1.0.2 requests wfuzz holehe twint droopescan uro arjun dnsgen s3scanner emailfinder pipx one-lin3r win_unicode_console aiodnsbrute webscreenshot dnspython netaddr
+    pip3 install --upgrade pip osrframework
+    pip3 install py-altdns==1.0.2 requests wfuzz holehe twint droopescan uro arjun dnsgen s3scanner emailfinder pipx one-lin3r win_unicode_console aiodnsbrute webscreenshot dnspython netaddr --retries 10
     gem install typhoeus opt_parse_validator blunder wpscan
     mkdir -p "$HOME/.local"
     : > $HOME/.local/.arno_init_install_successful
